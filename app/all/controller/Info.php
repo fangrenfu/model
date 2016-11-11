716783e9f400d4f28dbc0af2d2a2c6b76a8cb2ba
fangrenfu@126.com <comefirst0>
2016-11-11 08:58:50 +0800

    完成部分

--- app/all/controller/Info.php
+++ app/all/controller/Info.php
@@ -18,7 +18,7 @@ use app\common\access\MyAccess;
 class Info
 {
     //获取课名
-    public function  getcourseinfo($courseno)
+    public function  getcourseinfo($id)
     {
         $result = null;
         $status = 1;
@@ -26,7 +26,7 @@ class Info
         $credits=null;
         $hours=null;
         try {
-            $result = Item::getCourseItem($courseno, false);
+            $result = Item::getCourseItem($id, false);
             if ($result != null) {
                 $coursename= $result['coursename'];
                 $credits=$result['credits'];
@@ -40,64 +40,5 @@ class Info
             MyAccess::throwException($e->getCode(), $e->getMessage());
         }
         return json(["status" => $status, 'coursename' => $coursename,'hours'=>$hours,'credits'=>$credits]);
-    }
-    //获取教室名称
-    public function getroominfo($roomno)
-    {
-        $result = null;
-        $status = 1;
-        try {
-            $result = Item::getRoomItem($roomno,false);
-            if ($result != null) {
-                $result = $result['name'];
-                $status = 1;
-            } else {
-                $result = '该教室不存在!';
-                $status = 0;
-            }
-        } catch (\Exception $e) {
-            MyAccess::throwException($e->getCode(), $e->getMessage());
-        }
-        return json(["status" => $status, 'roomname' => $result]);
-    }
-    //获取教学计划名称
-    public function  getprograminfo($programno){
-        $result = null;
-        $status = 1;
-        try {
-            $result = Item::getProgramItem($programno);
-            if ($result != null) {
-                $result = $result['progname'];
-                $status = 1;
-            } else {
-                $result = '该教学计划不存在!';
-                $status = 0;
-            }
-        } catch (\Exception $e) {
-            MyAccess::throwException($e->getCode(), $e->getMessage());
-        }
-        return json(["status" => $status, 'progname' => $result]);
-    }
-
-    public function  getclassinfo($classno)
-    {
-        $result = null;
-        $status = 1;
-        $classname=null;
-        $schoolname=null;
-        try {
-            $result =Item::getClassItem($classno,false);
-            if ($result != null) {
-                $classname = $result['classname'];
-                $schoolname= $result['schoolname'];
-                $status = 1;
-            } else {
-                $classname= '该班级不存在!';
-                $status = 0;
-            }
-        } catch (\Exception $e) {
-            MyAccess::throwException($e->getCode(), $e->getMessage());
-        }
-        return json(["status" => $status, 'classname' => $classname,'schoolname'=>$schoolname]);
     }
 }
--- app/common/access/Item.php
+++ app/common/access/Item.php
@@ -19,176 +19,26 @@ use think\Exception;
  * @package app\common\access
  */
 class Item {
-    /**根据教室号获取教室信息
-     * @param $roomno
-     * @param bool $alert
-     * @return null
-     * @throws Exception
-     * @throws \think\exception\DbException
-     */
-    public static function  getRoomItem($roomno,$alert=true){
-        $condition=null;
-        $result=null;
-        $condition['roomno']=$roomno;
-        $data=Db::table('classrooms')->where($condition)->field('rtrim(jsn) name,status,reserved')->select();
-        if(!is_array($data)||count($data)!=1) {
-            if($alert)
-                throw new Exception('roomno:' . $roomno, MyException::ITEM_NOT_EXISTS);
-        }
-        else
-            $result=$data[0];
-        return $result;
-    }
+
 
     /**根据课号获取课程
-     * @param string $courseno
+     * @param string $id
      * @param bool $alert
      * @return null
      * @throws Exception
      * @throws \think\exception\DbException
      */
-    public static function getCourseItem($courseno,$alert=true){
+    public static function getCourseItem($id,$alert=true){
         $result=null;
         $condition=null;
-        $condition['courseno']=$courseno;
-        $data=Db::table('courses')->where($condition)->field('rtrim(coursename) as coursename,courseno,credits,hours')->select();
+        $condition['id']=$id;
+        $data=Db::table('courses')->where($condition)->field('rtrim(name) as name,id')->select();
         if(!is_array($data)||count($data)!=1) {
             if($alert)
-                throw new Exception('courseno:' . $courseno, MyException::ITEM_NOT_EXISTS);
+                throw new Exception('id:' . $id, MyException::ITEM_NOT_EXISTS);
         }
         else
             $result=$data[0];
         return $result;
-    }
-
-    /**获取教学计划
-     * @param $programno
-     * @param bool $alert
-     * @return null
-     * @throws Exception
-     */
-    public static function getProgramItem($programno,$alert=true){
-        $result=null;
-        $condition=null;
-        $condition['programno']=$programno;
-        $data=Db::table('programs')->where($condition)->field('rtrim(progname) as progname,programno')->select();
-        if(!is_array($data)||count($data)!=1) {
-            if($alert)
-                throw new Exception('programno:' . $programno, MyException::ITEM_NOT_EXISTS);
-        }
-        else
-            $result=$data[0];
-        return $result;
-    }
-
-    /**获取开课计划
-     * @param $year
-     * @param $term
-     * @param $courseno
-     * @param bool $alert
-     * @return null
-     * @throws Exception
-     */
-    public static function getSchedulePlanItem($year,$term,$courseno,$alert=true){
-        $result=null;
-        $condition=null;
-        $condition['scheduleplan.year']=$year;
-        $condition['scheduleplan.term']=$term;
-        $condition['scheduleplan.courseno+scheduleplan.[group]']=$courseno;
-        $data=Db::table('scheduleplan')
-            ->join('courses','courses.courseno=scheduleplan.courseno')->where($condition)
-            ->field('rtrim(coursename) as coursename,scheduleplan.courseno+scheduleplan.[group] courseno,
-            scheduleplan.estimate,scheduleplan.attendents,scheduleplan.year,scheduleplan.term')
-            ->select();
-        if(!is_array($data)||count($data)!=1) {
-            if($alert)
-                throw new Exception('year:' . $year.',term:'.$term.',courseno'.$courseno, MyException::ITEM_NOT_EXISTS);
-        }
-        else
-            $result=$data[0];
-        return $result;
-    }
-
-    /**获取教师
-     * @param $teacherno
-     * @param bool $alert
-     * @return null
-     * @throws Exception
-     */
-    public static function getTeacherItem($teacherno,$alert=true){
-        $result=null;
-        $condition=null;
-        $condition['teacherno']=$teacherno;
-        $data=Db::table('teachers')->where($condition)->field('rtrim(name) as teachername,teacherno')->select();
-        if(!is_array($data)||count($data)!=1) {
-            if($alert)
-                throw new Exception('teacherno:' . $teacherno, MyException::ITEM_NOT_EXISTS);
-        }
-        else
-            $result=$data[0];
-        return $result;
-    }
-
-    /**获取班级信息
-     * @param $classno
-     * @param bool $alert
-     * @return null
-     * @throws Exception
-     */
-    public static function getClassItem($classno,$alert=true){
-        $result=null;
-        $condition=null;
-        $condition['classno']=$classno;
-        $data=Db::table('classes')
-            ->join('schools','schools.school=classes.school')
-            ->field('rtrim(classname) as classname,classno,schools.school,rtrim(schools.name) schoolname')
-            ->where($condition)->select();
-        if(!is_array($data)||count($data)!=1) {
-            if($alert)
-                throw new Exception('classno:' . $classno, MyException::ITEM_NOT_EXISTS);
-        }
-        else
-            $result=$data[0];
-        return $result;
-    }
-
-    /**获取单双周信息
-     * @param $oew
-     * @return mixed
-     * @throws Exception
-     */
-    public static  function getOEWItem($oew){
-        $condition['code']=$oew;
-        $result=Db::table('oewoptions')->field('rtrim(name) name,timebit,timebit2')->where($condition)->select();
-        if(!is_array($result)||count($result)!=1)
-            throw new Exception('oew'.$oew, MyException::ITEM_NOT_EXISTS);
-        return $result[0];
-    }
-
-    /**获取时间段信息
-     * @param $time
-     * @return mixed
-     * @throws Exception
-     */
-    public static function getTimeSectionItem($time)
-    {
-        $condition['name']=$time;
-        $result=Db::table('timesections')->field('rtrim(value) value,timebits,timebits2')->where($condition)->select();
-        if(!is_array($result)||count($result)!=1)
-            throw new Exception('time'.$time,  MyException::ITEM_NOT_EXISTS);
-        return $result[0];
-    }
-    //获取某个开设专业
-    public static function getMajorItem($majorschool)
-    {
-        $condition['majorschool']=$majorschool;
-        $result=Db::table('majors')->join('schools','schools.school=majors.school')
-            ->join('majorcode','majorcode.code=majors.majorno')
-            ->join('majordirection','majordirection.direction=majors.direction')
-            ->field('majors.majorschool,rtrim(schools.name) schoolname,majors.direction,rtrim(majordirection.name) directionname,
-            majors.majorno,rtrim(majorcode.name) majorname,majors.rowid')->where($condition)->select();
-        if(!is_array($result)||count($result)!=1)
-            throw new Exception('majorschool'.$majorschool,  MyException::ITEM_NOT_EXISTS);
-        return $result[0];
     }
 }
